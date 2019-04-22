6a2594b425d15adbc359ed4b8a425dc0c99bce69
Maksim Black <maksim@MacBook-Pro-Maksim.local>
2019-04-22 17:31:57 +0300

    Add yum config

--- bash/centos/docker.sh
+++ bash/centos/docker.sh
@@ -4,6 +4,7 @@ echo "Installing Docker"
 
 yum -y update
 yum -y install yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum -y install docker-ce
 systemctl start docker
 systemctl enable docker
