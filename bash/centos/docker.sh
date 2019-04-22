fecfb4817e66916a91316ecd03ce19a6232daa5a
Maksim Black <maksim@MacBook-Pro-Maksim.local>
2019-04-22 17:42:03 +0300

    Add kubernetes

--- bash/centos/docker.sh
+++ bash/centos/docker.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-echo "Installing Docker"
-
+echo "== ██████╗  ██████╗  ██████╗██╗  ██╗███████╗██████╗  =="
+echo "== ██╔══██╗██╔═══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗ =="
+echo "== ██║  ██║██║   ██║██║     █████╔╝ █████╗  ██████╔╝ =="
+echo "== ██║  ██║██║   ██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗ =="
+echo "== ██████╔╝╚██████╔╝╚██████╗██║  ██╗███████╗██║  ██║ =="
+echo "== ╚═════╝  ╚═════╝  ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝ =="
+                                                                                                                                 
 yum -y update
 yum -y install yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum -y install docker-ce
-systemctl start docker
-systemctl enable docker
-systemctl status docker
+systemctl start docker && systemctl enable docker
 firewall-cmd --permanent --zone=trusted --change-interface=docker0
 firewall-cmd --permanent --zone=trusted --add-port=4243/tcp
 firewall-cmd --reload
