e45a92e0e172cda314262e8201994a7f308b27fd
Максим Чёрный <mbkkong@gmail.com>
2019-05-13 12:54:40 +0300

    Changes

--- bash/add_docker_bashrc.sh
+++ bash/add_docker_bashrc.sh
@@ -10,9 +10,9 @@ alias dpa="docker ps -a"
 alias di="docker images"
 alias dcup="docker-compose up --build -d"
 alias drmall="docker system prune --all --force --volumes"
-dstop() { docker stop $(docker ps -a -q); }
-drm() { docker rm $(docker ps -a -q); }
-dlogs() { docker logs -f $1; }
-dsh() { docker exec -it $1 /bin/sh; }
-dbash() { docker exec -it $1 /bin/bash; }
+dstop() { docker stop \$(docker ps -a -q); }
+drm() { docker rm \$(docker ps -a -q); }
+dlogs() { docker logs -f \$1; }
+dsh() { docker exec -it \$1 /bin/sh; }
+dbash() { docker exec -it \$1 /bin/bash; }
 EOF
